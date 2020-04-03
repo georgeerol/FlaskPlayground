30f61b77d8d3c38f696459f0e80b25b152eadc8d
George Fouche <fouliex@gmail.com>
2020-04-03 13:23:05 -0700

    set sqalchemy tables

--- sqlalchemy/app.py
+++ sqlalchemy/app.py
@@ -7,6 +7,7 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
 api = Api(app)
@@ -18,4 +19,7 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
+
+    db.init_app(app)
     app.run(debug=True)  # important to mention debug=True
